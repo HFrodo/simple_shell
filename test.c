a19df2fe871765abcecf056f45b5cf484fa2c6e0
HFrodo <egyepong10@gmail.com>
2024-08-20 21:33:03 +0000

    testing betty

--- test.c
+++ test.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a sentence
+ *
+ * Description: Outputs a greeting message to the screen
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("Hello, my name is David\n");
 
-	return(0);
+	return (0);
 }
